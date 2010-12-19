7a1e42e5cfd49db631a5e5ce1376aa0bfc4b5825
patmoore <patmoore@6eaf30a2-012b-4626-ba8e-69dab18af4f2>
2010-12-19 03:03:09 +0000

    now using amplafi-flow-client (in prep for amplafi-android)
    seems appropriate to bump version number
    
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-flow-client/trunk@904 6eaf30a2-012b-4626-ba8e-69dab18af4f2

---
+++ src/main/java/org/amplafi/flow/definitions/DefinitionSource.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.amplafi.flow.definitions;
+
+import java.util.Map;
+
+/**
+ * Implementers will provide Flow definitions.
+ *
+ * TODO should be namespace owner.
+ * @author patmoore
+ * @param <F>
+ *
+ */
+public interface DefinitionSource<F extends FlowDefinition> {
+
+    /**
+     * Returns the flow having the specified name.
+     * @param flowTypeName
+     * @return the Flow definition. null if {@link #isFlowDefined(String)} returns false.
+     */
+    F getFlowDefinition(String flowTypeName);
+    /**
+     * does this DefinitionSource have a definition for a flow with supplied name.
+     * @param flowTypeName
+     * @return true if definition exists.
+     */
+    boolean isFlowDefined(String flowTypeName);
+
+    /**
+     * Returns all defined flows, keyed by their name.
+     * @return the map with all the currently defined flows indexed by (usually) the {@link org.amplafi.flow.Flow#getFlowPropertyProviderName()}.
+     */
+    Map<String, F> getFlowDefinitions();
+}
---
+++ src/main/java/org/amplafi/flow/definitions/MapDefinitionSource.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.amplafi.flow.definitions;
+
+import java.util.Map;
+
+/**
+ * Simple Flow {@link DefinitionSource}
+ * @author patmoore
+ * @param <F>
+ *
+ */
+public class MapDefinitionSource<F extends FlowDefinition> implements DefinitionSource<F> {
+    private Map<String, F> flowDefinitions;
+
+    public MapDefinitionSource() {
+
+    }
+    /**
+     * @param flowDefinitions
+     */
+    public MapDefinitionSource(Map<String, F> flowDefinitions) {
+        this.flowDefinitions = flowDefinitions;
+    }
+    /**
+     * @see org.amplafi.flow.definitions.DefinitionSource#getFlowDefinition(java.lang.String)
+     */
+    public F getFlowDefinition(String flowTypeName) {
+        return this.flowDefinitions != null? this.flowDefinitions.get(flowTypeName):null;
+    }
+
+
+    /**
+     * @see org.amplafi.flow.definitions.DefinitionSource#isFlowDefined(java.lang.String)
+     */
+    public boolean isFlowDefined(String flowTypeName) {
+        return this.flowDefinitions != null?  this.flowDefinitions.containsKey(flowTypeName): false;
+    }
+
+
+    /**
+     * @param flowDefinitions the flowDefinitions to set
+     */
+    public void setFlowDefinitions(Map<String, F> flowDefinitions) {
+        this.flowDefinitions = flowDefinitions;
+    }
+
+
+    /**
+     * @return the flowDefinitions
+     */
+    public Map<String, F> getFlowDefinitions() {
+        return flowDefinitions;
+    }
+
+}
