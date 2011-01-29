fb6b9562aedcfb62f97de53c32dc3a7ecaea7b6a
aectann <aectann@aelinux.site>
2011-01-29 12:20:22 +0800

    Common request parameters enum.

---
+++ src/main/java/org/amplafi/flow/auth/StandardFlowRequestParameters.java
@@ -0,0 +1,13 @@
+package org.amplafi.flow.auth;
+
+/**
+ * Some common flow request parameters.
+ * 
+ * @author Konstantin Burov
+ *
+ */
+public enum StandardFlowRequestParameters {
+
+	CLIENT_USER_ID;	
+	
+}
