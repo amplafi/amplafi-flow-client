71672170845fa21e80a7bd19da57652484ad7a8e
Haris Osmanagic <haris.osmanagic@gmail.com>
2012-09-28 01:31:55 +0200

    refactored version using Commons CLI. hardcoded values rremoved.

--- src/main/java/org/amplafi/flow/utils/CommandLineClient.java
+++ src/main/java/org/amplafi/flow/utils/CommandLineClient.java
@@ -1,11 +1,17 @@
 package org.amplafi.flow.utils;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import static org.amplafi.flow.utils.CommandLineClientOptions.API_KEY;
+import static org.amplafi.flow.utils.CommandLineClientOptions.API_VERSION;
+import static org.amplafi.flow.utils.CommandLineClientOptions.FLOW;
+import static org.amplafi.flow.utils.CommandLineClientOptions.HOST;
+import static org.amplafi.flow.utils.CommandLineClientOptions.PARAMS;
+import static org.amplafi.flow.utils.CommandLineClientOptions.PORT;
 
-import org.apache.commons.lang3.ArrayUtils;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
 
 /**
  * In lieu of an andriod client.
@@ -14,100 +20,50 @@ import org.apache.commons.lang3.ArrayUtils;
  */
 
 public class CommandLineClient implements Runnable {
-	private final String requestUriString;
-
-	private final String apiKey;
-	
-	private final String flow;
-	
-	// command line switch used to specify which API key we want to use
-	private static final String API_KEY_CMD_SWITCH = "-key";
-		
-	// command line switch used to specify which flow we want to use
-	private static final String FLOW_CMD_SWITCH = "-flow";
-	
+	private FarReachesServiceInfo serviceInfo;
+	private CommandLineClientOptions cmdOptions;
+	private List<NameValuePair> queryParams = new ArrayList<NameValuePair>();
 	
 	public static void main(String[] args) {
-//	    HACK TO_HARIS: doesn't looks like a convenient way to parse command line parameters can we use something like
-//	          http://commons.apache.org/cli/ ??
-//	     
-		Map<String, String> params = extractParams(args);
-		
-		if (params == null) {
-			System.err.println("Invalid input arguments, application will stop.");
-			System.exit(0);
-		}
-		
-		CommandLineClient client = new CommandLineClient(params);
-		client.run();		
+		CommandLineClientOptions cmdOptions = new CommandLineClientOptions(args);
+
+		FarReachesServiceInfo serviceInfo = new FarReachesServiceInfo(
+				cmdOptions.getOptionValue(HOST),
+				cmdOptions.getOptionValue(PORT),
+				cmdOptions.getOptionValue(API_VERSION));
+
+		CommandLineClient client = new CommandLineClient(serviceInfo, cmdOptions);
+		client.run();
 	}
-	
-	public CommandLineClient(Map<String, String> params) {
-	    //HACK TO_HARIS: apiKey and flow got to be separate (mandatory) parameters to the constructor.
-	    //We need a cleaner interface to later reuse this class in other tasks.
-		this.apiKey = params.get(API_KEY_CMD_SWITCH);
-		this.flow = params.get(FLOW_CMD_SWITCH);
+
+	// TO_HARIS Replace cmdOptions with a flow definition and params
+	// TO_HARIS Extract query params to the flow call
+	// For now, parameters are required to be a ready-made query (i.e. in the form of ?param1=value1&param2=value2).
+	// Params should be a list of name-value pairs, 
+	// so that the caller of the constructor doesn't have to build the query himself
+	public CommandLineClient(FarReachesServiceInfo serviceInfo,
+			CommandLineClientOptions cmdOptions) {
 		
-		this.requestUriString = buildRequestUriString();
+		this.serviceInfo = serviceInfo;
+		this.cmdOptions = cmdOptions;
 	}
 
 	private String buildRequestUriString() {
-	    //HACK TO_HARIS: Why is server address hardcoded? 
-		return "http://sandbox.farreach.es:8080/c/"
-				+ apiKey + "/apiv1/" 
-				+ flow;
-	}
-
-	/**
-	 * HACK TO_HARIS: doesn't looks like a convenient way to parse command line parameters can we use something like
-	 *     http://commons.apache.org/cli/ ??
-	 * 
-	 * Extract parameters from user's input. Every switch (-key, -flow...) has to be followed by a value.
-	 * @param args
-	 * @return
-	 */
-	private static Map<String, String> extractParams(String[] args) {
-		if (ArrayUtils.isEmpty(args)) {
-			return null;
+		String fullUri =  serviceInfo.getHost()  + ":" + serviceInfo.getPort() + "/c/"
+				+ cmdOptions.getOptionValue(API_KEY) 
+				+ "/" + serviceInfo.getApiVersion()  
+				+ "/" + cmdOptions.getOptionValue(FLOW); 
+ 
+		if (cmdOptions.getOptionValue(PARAMS) != null) {
+			fullUri += "/" + cmdOptions.getOptionValue(PARAMS);
 		}
 		
-		Map<String, String> params = new HashMap<String, String>();
-		
-		for (int i = 0; i < args.length; i++) {
-		    //
-			if (API_KEY_CMD_SWITCH.equals(args[i])) {
-				if (i == args.length - 1) {
-					System.err.println("No API key specified");
-					params = null;
-					break;
-				}
-				
-				params.put(API_KEY_CMD_SWITCH, args[i+1]);
-				//skip parameter value, continue to next parameter name
-				i++;
-			} else if (FLOW_CMD_SWITCH.equals(args[i])) {
-				if (i == args.length - 1) {
-					System.err.println("No flow specified");
-					params = null;
-					break;
-				}
-				
-				params.put(FLOW_CMD_SWITCH, args[i+1]);
-				//skip parameter value, continue to next parameter name
-				i++;
-			} else {
-				System.err.println("Unknown switch");
-				params = null;
-				break;
-			}
-		}
-		
-		return params;
+		return fullUri;
 	}
 
 	public void run() {
-	    //HACK TO_HARIS: How to supply parameters to the flow call?
-		GeneralFlowRequest flowRequest = new GeneralFlowRequest(URI.create(requestUriString), Collections.EMPTY_LIST);
+		GeneralFlowRequest flowRequest = new GeneralFlowRequest(
+				URI.create(buildRequestUriString()), queryParams);
 		System.out.println(flowRequest.get());
 	}
 
