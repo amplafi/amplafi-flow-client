72668401ef23fd020cb79a7b55cb57918c110f99
tuan08 <tuan08@gmail.com>
2013-01-21 11:27:02 +0700

    TO_KOSTYA

--- src/main/resources/commandScripts/CustomerProblemReport.groovy
+++ src/main/resources/commandScripts/CustomerProblemReport.groovy
@@ -12,17 +12,20 @@ description "CustomerProblemReport", "Params: customerEmail=<customer_email> Thi
 // setKey("newkey");
 
 def verbose = false ; 
-if (params && params["verbose"]) {
-    verbose = params["verbose"] ;
+println "verbose = " + params["verbose"] ;
+if (params&&params["verbose"]) {
+    verbose = "true".equals(params["verbose"]) ;
 }
 
-//TO_TUAN wtf? Why defult to this email?
-def customerEmail = "tuan08@gmail.com"
+println "customerEmail = " + params["customerEmail"] ;
+def customerEmail = "admin@amplafi.com"
 if (params && params["customerEmail"]) {
     customerEmail = params["customerEmail"] ;
 }
 
-println "Find the api key for the customer " + customerEmail ;
+println "----------------------------------------------------------------------------------"
+println "Create the temporaty api key for the customer " + customerEmail ;
+println "----------------------------------------------------------------------------------"
 
 setApiVersion("suv1");
 request("SuApiKeyFlow", ["fsRenderResult":"json", "email": customerEmail, "reasonForAccess": "Inspect the customer problem"]);
@@ -38,24 +41,122 @@ if(apiKey == null) {
     prettyPrintResponse();
     System.exit(0) ;
 }
-
 println "Create a temporary api key for the user " + customerEmail + ": " + apiKey ;
 
+//TO_KOSTYA and TO_PAT: Here are the list of the avaliable flows in the apiv1. I would like to run:
+//1. List the avaiable social account(ext service) and the configured categories of each social account(ext service)
+//2. Customer role base on the customer email
+//Which flow and parameters that I should use
+
+//"AccountCalendarInformation",
+//"AccountContactInformation",
+//"AccountExternalServiceInformation",
+//"AccountGeneralInformation",
+//"AccountLocationInformation",
+//"AccountMemberInformation",
+//"ActivateMessageEndPoint",
+//"ApprovedList",
+//"AvailableCategoriesFlow",
+//"BroadcastTopicEntry",
+//"BroadcastTopicMessageEndPoints",
+//"CategoriesList",
+//"CategoryFlow",
+//"CategoryMessages",
+//"ChangePassword",
+//"ChangeTopicFrequency",
+//"ClearFlows",
+//"ConfigureExtServices",
+//"ConfigureMessagePointCategories",
+//"CreateAlert",
+//"CreateCancel",
+//"CreateComment",
+//"CreateEvent",
+//"CreateMessage",
+//"CreateRevoke",
+//"DraftMessages",
+//"EditIndividualBroadcastProviderUserProfile",
+//"EditUser",
+//"EditUserProfile",
+//"EligibleDestinationMessageEndPointListFlow",
+//"EligibleDestinationMessageEndPointListMapFlow",
+//"EligibleExternalServiceInstanceMapFlow",
+//"EligibleExternalServiceInstancesFlow",
+//"EnterVerificationCode",
+//"EnvelopeStatusesFlow",
+//"ExternalServicesCredentials",
+//"FindBroadcastProviderFlow",
+//"FinishSignUp",
+//"ForgetMessageEndPoint",
+//"ForgotPassword",
+//"GeneralUserSettings",
+//"GetWordpressPlugin",
+//"GetWordpressPluginInfo",
+//"InactivateMessageEndPoint",
+//"IntroductoryFlow",
+//"Login",
+//"Logout",
+//"MessageEndPointCloudDataMapFlow",
+//"MessageEndPointListFlow",
+//"MessagePointEnvelopeRecordsFlow",
+//"NewTemplate",
+//"OurInformation",
+//"PendingList",
+//"PermanentApiKey",
+//"RefreshDerivedConnections",
+//"ResetApiKeysFlow",
+//"SelectPartnerTopics",
+//"SignupBusiness",
+//"SignupGovernment",
+//"SignupIndividual",
+//"SignupOrganization",
+//"SignupPolitical",
+//"TemplatesList",
+//"TemporaryApiKeyFlow",
+//"UnpublishToDraft",
+//"UserCalendarSettings",
+//"UserEmailSettings",
+//"UserPasswordSettings",
+//"VerifyEmailOwnership",
+//"VerifyUrlOwnership",
+//"WelcomeFromEmail"
+setApiVersion("apiv1");
+println "----------------------------------------------------------------------------------"
+println "Account External Service Information"
+println "----------------------------------------------------------------------------------"
+request("AccountExternalServiceInformation", ["fsRenderResult":"json"]);
+prettyPrintResponse();
+
+println "----------------------------------------------------------------------------------"
+println "Avaliable Categories"
+println "----------------------------------------------------------------------------------"
+request("AvailableCategoriesFlow", ["fsRenderResult":"json"]);
+prettyPrintResponse();
+
+println "----------------------------------------------------------------------------------"
+println "Categories List"
+println "----------------------------------------------------------------------------------"
+request("CategoriesList", ["fsRenderResult":"json"]);
+prettyPrintResponse();
+
+println "----------------------------------------------------------------------------------"
+println "ApiRequestAuditEntry log"
+println "----------------------------------------------------------------------------------"
 setApiVersion("suv1");
 def httpStatusCodeList = "[0, 400, 401]" ;
-request("ApiRequestAuditEntriesFlow", ["fsRenderResult":"json", "email": customerEmail, "httpStatusCodeList": httpStatusCodeList]);
+request("ApiRequestAuditEntriesFlow", ["fsRenderResult":"json", "email": customerEmail, "httpStatusCodeList": httpStatusCodeList, "maxReturn": "10"]);
 if(verbose) {
     prettyPrintResponse();
 } else {
     def entries = getResponseData();
     if(entries instanceof JSONArray) {
-        println sprintf('%1$3s %2$10s %3$30s %4$30s', '#', 'Http Status', 'Path', 'Message') ;
+        def tabularTmpl = '%1$3s %2$10s %3$35s %4$35s' ;
+        println sprintf(tabularTmpl, '#', 'Http Status', 'Path', 'Message') ;
         for(int i = 0; i < entries.length(); i++) {
             def entry = entries.get(i) ;
             def path = entry.getStringByPath("request.parameters.requestPathArray") ;
             def responseHttpStatus = entry.getStringByPath("response.code") ;
             def message = entry.getStringByPath("message") ;
-            println sprintf('%1$3s %2$10s %3$30s %4$30s', i + 1, responseHttpStatus, path, message) ;
+            println sprintf(tabularTmpl, i + 1, responseHttpStatus, path, message) ;
         }
     } else {
         prettyPrintResponse();
