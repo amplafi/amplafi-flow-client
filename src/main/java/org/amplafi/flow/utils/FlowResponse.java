fe43e613558f7fbfadddc62024723b1ec4f3ce21
paul <paul_smout@bizpoint.com.sg>
2013-03-03 18:45:43 +0800

    added reponse explanations

--- src/main/java/org/amplafi/flow/utils/FlowResponse.java
+++ src/main/java/org/amplafi/flow/utils/FlowResponse.java
@@ -1,7 +1,8 @@
 package org.amplafi.flow.utils;
 
 import static org.amplafi.flow.utils.GeneralFlowRequest.APPLICATION_ZIP;
-
+import java.util.Map;
+import java.util.HashMap;
 import org.amplafi.json.JSONArray;
 import org.amplafi.json.JSONObject;
 import org.apache.http.Header;
@@ -9,11 +10,21 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 
 public class FlowResponse {
+        
     private HttpResponse response ;
     private String responseText ;
+    private Map<Integer,String> responseExplanations = new HashMap<Integer,String>(); 
     
     public FlowResponse(HttpResponse response) {
         this.response = response ;
+
+        responseExplanations.put(200, "A call finished successfully. A call result (an object, an array, or a string might be returned as response body");
+        responseExplanations.put(400, "An error (usually user related) happened on server. Usually means bad request parameters.");
+        responseExplanations.put(404, "Flow not found. Means that the request tried to access a non existent API entry point.");
+        responseExplanations.put(401, "Authorization problem. Usually means you're using invalid API key.");
+        responseExplanations.put(500, "Server has problems. Contact server developers.");
+        responseExplanations.put(302, "Redirect. Usually not handled by client, as redirects happen automatically.");
+        
     }
     
     public HttpResponse getHttpResponse() {
@@ -61,6 +72,9 @@ public class FlowResponse {
     }
     
     public String getErrorMessage() {
-        return "Http Status Code " + this.getHttpStatusCode() + "\n" + this.getResponseAsString() ;
+        return "Http Status Code " +
+                this.getHttpStatusCode() + "\n" +
+                    responseExplanations.get(this.getHttpStatusCode()) + "\n" +
+                        this.getResponseAsString() ;
     }
 }
