f2c64902310d77bcc1ce56d3b8c3d8a669ac7c90
yanyubing <daisy@bizpoint.cn>
2013-03-15 17:34:21 +0800

    get new api key script updates

--- src/main/resources/commandScripts/GetNewPermanentKey.groovy
+++ src/main/resources/commandScripts/GetNewPermanentKey.groovy
@@ -18,8 +18,16 @@ def key = openPort(9123,10,{
     requestPost("PermanentApiKey", ["callbackUri":"http://${callbackHost}:9123/?action=farreaches_server_reply&apiCall=PermanentApiKey&externalUserId=1&farreachesNonce=54127ddbe5",
                                     "action":"farreaches_server_reply",
                                     "apiCall":"PermanentApiKey",
-                                    "usersList":"[{'email':'test@example.com','roleType':'adm','displayName':'user','externalId':1}]",
-                                    "temporaryApiKey":tempApiKey
+                                    "usersList":"[{'email':'admin@amplafi.com','roleType':'adm','displayName':'user','externalId':1}]",
+                                    "temporaryApiKey":tempApiKey,
+                                    "logonBroadcastProvider":"2",
+                                    "logonBroadcastProviderLookupKey": "bpr_9f688fc3-c5e5-45ce-a05f-681be979d7af",
+                                    "security_level": "IDENTIFIED",
+                                    "externalUserId": "1",
+                                    "defaultLanguage": "en",
+                                    "selfName": "user's Blog!",
+                                    "requestPathArray": "PermanentApiKey",
+                                    "completeList": "true"
                                     ])
     }
     , { request, response ->
@@ -28,6 +36,8 @@ def key = openPort(9123,10,{
             return new JSONObject(permanentKey).get("1");
         }
     )
+    
+setKey(key);
 
 emitOutput( "NEW KEY IS : " + key );
 
