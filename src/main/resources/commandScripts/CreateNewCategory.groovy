0e21c36ce3367426929793658ea014b4e410d225
yanyubing <daisy@bizpoint.cn>
2013-04-16 16:47:30 +0800

    update create new category script

--- src/main/resources/commandScripts/CreateNewCategory.groovy
+++ src/main/resources/commandScripts/CreateNewCategory.groovy
@@ -4,13 +4,14 @@
 description "CreateNewCategory", "create a new category", [paramDef("topicName","The name of the category topic name",true,"How To use java"),
                                                             paramDef("topicTag","The tag of the category topic",true,"My first java application:hello world!"),
                                                             paramDef("topicAdditionalDescription","The description of the category topic",true,"class,function,main"),
-                                                            paramDef("externalContentId","The external content id",true,"[85,'tag']")];
-
+                                                            paramDef("externalContentId","The external content id",true,"[88,'tag']")];
+setApiVersion("api");
 def response = requestPost("Category",[
                                         "topicName":topicName,
                                         "topicTag":topicTag,
                                         "topicAdditionalDescription":topicAdditionalDescription,
                                         "externalContentId":externalContentId]);
 
+println "response" + response;
 
 return response;
