9bb25cb716e1c63d463225263b7198b20cb210b6
Patrick Moore <patmoore@amplafi.com>
2013-05-20 12:46:01 -0700

    remove commented out code

--- src/main/resources/sqlScripts/failedPublishingProviders.sql
+++ src/main/resources/sqlScripts/failedPublishingProviders.sql
@@ -10,14 +10,3 @@ where CALLBACKS.LOGON_USER = AUDIT_API_REQUEST.LOGON_USER
 and PROVIDERS.ID = CALLBACKS.LOGON_BROADCAST_PROVIDER
 and AUDIT_API_REQUEST.HTTP_STATUS_CODE not in(0,200)
 and AUDIT_API_REQUEST.USER_EMAIL is not null;
-
-/*
-select distinct providers.*
-from providers 
-INNER JOIN callbacks
-on providers.ID = callbacks.LOGON_BROADCAST_PROVIDER
-and callbacks.LOGON_USER in (select audit_api_request.LOGON_USER
-                             from audit_api_request 
-                             where audit_api_request.HTTP_STATUS_CODE <> 0
-                             and audit_api_request.USER_EMAIL is not null);
-*/
