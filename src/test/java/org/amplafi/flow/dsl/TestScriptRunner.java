6eceb1c13a3175573072813eda04ad189acf88cc
Konstantin Burov <aectann@gmail.com>
2013-06-18 16:47:36 +1000

    Removed tests that tested removed code. Fixed compilation errors.

--- src/test/java/org/amplafi/flow/dsl/TestScriptRunner.java
+++ src/test/java/org/amplafi/flow/dsl/TestScriptRunner.java
@@ -29,7 +29,7 @@ public class TestScriptRunner {
 
         String script = "println('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFf'); \n " +
                     " request('HelloFlow',['cat':'dog','hippo':'pig']);" ;
-        instance.runScriptSource(script,true,null);
+        instance.runScriptSource(script,null);
     }
 
     @Test
