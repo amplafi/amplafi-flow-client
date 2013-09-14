2b9cb2447e22bc4dbb89285b92e7b2cf7d7f26ff
Konstantin Burov <aectann@gmail.com>
2013-09-14 12:32:58 +1000

    Removed unused code.

--- src/main/java/org/amplafi/flow/ui/command/RunScriptCommand.java
+++ src/main/java/org/amplafi/flow/ui/command/RunScriptCommand.java
@@ -1,21 +1,15 @@
 package org.amplafi.flow.ui.command;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.amplafi.flow.utils.AdminTool;
 
 /**
  * @author bfv
  * Command that interacts with the ScriptRunner and calls a particular script.
  */
-public class RunScriptCommand extends AShellCommand {
+public abstract class RunScriptCommand extends AShellCommand {
 
-	static final Pattern NO_SPACE_WORD = Pattern.compile("^([^\\s]*)$");
-	
 	public RunScriptCommand(String setOptions) {
 		super(setOptions);
-		
 	}
 	
 	@Override
@@ -26,17 +20,6 @@ public class RunScriptCommand extends AShellCommand {
 		return 0;
 	}
 
-    protected String getScriptName() {
-        String script = null;
-		String st = this.getOptions();
-		Matcher m = NO_SPACE_WORD.matcher(st);
-		if(m.matches()){
-			script = m.group(1);
-		}else{
-			System.out.println("Invalid options. Specify the name of a script. To see script names "
-					+ "use command \"lists\"");
-		}
-        return script;
-    }
+    abstract String getScriptName();
 
 }
