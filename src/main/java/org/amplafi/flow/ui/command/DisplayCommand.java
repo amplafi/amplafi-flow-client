e6c0b40d73d659bb2ba1987e0c21c14cfb82d439
Patrick Moore <patmoore@farreach.es>
2013-10-15 20:13:28 -0700

    remove old comment

--- src/main/java/org/amplafi/flow/ui/command/DisplayCommand.java
+++ src/main/java/org/amplafi/flow/ui/command/DisplayCommand.java
@@ -3,7 +3,6 @@ package org.amplafi.flow.ui.command;
 import org.amplafi.flow.utils.AdminTool;
 
 /**
- * @author bfv
  * A command to print something to console output.
  */
 public class DisplayCommand extends AbstractShellCommand {
@@ -14,8 +13,6 @@ public class DisplayCommand extends AbstractShellCommand {
 
 	@Override
 	public void execute(AdminTool adminTool) {
-		//TODO (Bruno) refactor this and other direct calls to sysste.out to a
-		//			   stream belonging to AdminTool.
 		System.out.println(getOptions());
 	}
 
